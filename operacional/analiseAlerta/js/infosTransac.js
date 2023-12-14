94982073d4d2feaf42f510720b3bb26d1a139b87
luks-reiis-MS <lucas.reis@msbrazil.com>
2023-12-14 12:33:08 -0300

    Add legenda no grafico de conta cliente de infos de transações de analise de alerta de operacional

--- operacional/analiseAlerta/js/infosTransac.js
+++ operacional/analiseAlerta/js/infosTransac.js
@@ -1,11 +1,11 @@
 var volumeTransacoesCtx = document.getElementById('volumeTransacoes').getContext('2d');
 
 var dataVolumeTransacoes = {
-    labels: ['Jul', 'Ago', 'Ste', 'Out', 'Nov', 'Dez'],
+    labels: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun'],
     datasets: [{
         label: 'Operações',
         type: 'bar',
-        backgroundColor: 'rgb(9,41,104)',
+        backgroundColor: 'rgb(068,114,196)',
         data: [2500, 3000, 4200, 4000, 5700, 6000],
         borderWidth: 1,
         pointRadius: 0,
@@ -85,4 +85,13 @@ var volumeTransacoes = new Chart(volumeTransacoesCtx, {
     type: 'line',
     data: dataVolumeTransacoes,
     options: optionsVolumeTransacoes
+});
+
+
+var customLegendVolumeTransacoes = document.getElementById('legenda-volume-transacoes');
+dataVolumeTransacoes.datasets.forEach(function(dataset, index) {
+    if(dataset.label == 'Operações')
+        customLegendVolumeTransacoes.innerHTML += '<div class="alinhamento-icone-palavra"><span style="background-color: rgb(068,114,196); width: 25px; height: 5px; display: inline-block; margin-right: 5px;"></span>' + dataset.label + '</div>';
+    else
+    customLegendVolumeTransacoes.innerHTML += '<div class="alinhamento-icone-palavra"><span style="background-color:' + dataset.borderColor + '; width: 25px; height: ' + dataset.borderWidth + 'px; display: inline-block; margin-right: 5px;"></span>' + dataset.label + '</div>';
 });
